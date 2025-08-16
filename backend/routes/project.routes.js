e748be7825692fd2a7bcdf3c448e7eb8d5ef7561
Aakanksha Sharma <122429931+aakanksha80@users.noreply.github.com>
2025-08-16 18:02:33 +0530

    Renaming Project Cnotroller to Controller

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -1,6 +1,6 @@
 const routerP = require('express').Router();
 const authenticateP = require('../middleware/auth');
-const { authorizeRoles: authRolesP } = require('../middleware/roles');
+const { authorizeRoles: authRolesP } =  require('../middleware/roles');
 const { createProject, getProjectsByTeam } = require('../controllers/project.controller');
 
 routerP.post('/', authenticateP, authRolesP('admin', 'manager'), createProject);
